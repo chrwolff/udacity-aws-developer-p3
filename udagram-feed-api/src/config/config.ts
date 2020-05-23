ffada3fcce714af699488aa1b7f68dfde4430a83
chrwolff <chrwolff@gmx.net>
2020-05-23 06:06:33 +0200

    moved secrets to kubectl store

--- udagram-feed-api/src/config/config.ts
+++ udagram-feed-api/src/config/config.ts
@@ -7,7 +7,6 @@ export const config = {
   'host': process.env.DB_HOST,
   'dialect': process.env.DB_DIALECT,
   'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
   'aws_media_bucket': process.env.AWS_MEDIA_BUCKET,
   'url': process.env.URL,
   'port': process.env.PORT || 8080, // default port to listen
